3b55dcf53af68a29327a06660273652aade49511
avatar80 <avatar80@naver.com>
2017-05-29 00:07:30 +0900

    trading책 연습문제 풀이

---
+++ trading_example.py
@@ -0,0 +1,29 @@
+import os
+
+# https://wikidocs.net/3144 연습문제
+''' 5-1
+def myaverage(a, b):
+    return (a+b)/2
+'''
+
+''' 5-2
+def get_max_min(data_list):
+    return max(data_list), min(data_list)
+myList = [ 3,4,7,4,12,8 ]
+print(get_max_min(myList))
+'''
+
+''' 5-3
+def get_txt_list(path):
+    myList = []
+    for x in os.listdir(path):
+        if x.endswith('txt'):
+            myList.append(x)
+    return myList
+print(get_txt_list('/home/avatar/Study/Python/study'))
+'''
+
+
+
+
+
